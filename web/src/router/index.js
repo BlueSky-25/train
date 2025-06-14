bea568c386f3906f108b0b1d3e5d4cecba205dcb
wanyang <wanyang1025@gamil.com>
2025-06-14 16:25:30 +0800

    1 . 加入axios等

--- web/src/router/index.js
+++ web/src/router/index.js
@@ -1,24 +1,26 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
 
 const routes = [
   {
     path: '/',
-    name: 'home',
-    component: HomeView
-  },
-  {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
+    component: () => import('../views/main.vue'),
+    meta: {
+      loginRequire: true
+    },
+    children: [{
+      path: 'welcome',
+      component: () => import('../views/main/welcome.vue'),
+    }
+    ]
   },
   {
     path: '/login',
     name: 'login',
-    component: () => import('../views/UserLogin.vue')
+    component: () => import('../views/login.vue')
+  },
+  {
+    path: '',
+    redirect: '/welcome'
   }
 ]
 
