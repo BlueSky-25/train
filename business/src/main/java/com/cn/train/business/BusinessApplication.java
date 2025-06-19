42947fe195018a5c8785d6a3ba542d41c92a1d81
wanyang <wanyang1025@gamil.com>
2025-06-19 15:05:48 +0800

    1 . 前端admin模块 2. 后端business模块新增

---
+++ business/src/main/java/com/cn/train/business/BusinessApplication.java
@@ -0,0 +1,29 @@
+package com.cn.train.business;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.env.ConfigurableEnvironment;
+
+/**
+ * @className: BusinessApplication
+ * @author: wanyang
+ * @date: 2025/6/19 14:47
+ * @version: 1.0
+ * @description: TODO
+ */
+@SpringBootApplication(scanBasePackages = {"com.cn.train.common", "com.cn.train.business"})
+@MapperScan("com.cn.train.business.mapper")
+public class BusinessApplication {
+
+    private static final Logger LOG = LoggerFactory.getLogger(BusinessApplication.class);
+
+    public static void main(String[] args) {
+        SpringApplication application = new SpringApplication(BusinessApplication.class);
+        ConfigurableEnvironment environment = application.run(args).getEnvironment();
+        LOG.info("启动成功 --> {}", BusinessApplication.class.getName());
+        LOG.info("地址: \t http://127.0.0.1:{}", environment.getProperty("server.port"));
+    }
+}
